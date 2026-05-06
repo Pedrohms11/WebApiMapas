a899c23e510506d9b35dddb634ce0436a8c444d1
Pedro Moura <Pedromoura32@gmail.com>
2026-05-05 21:50:30 -0300

    Implementando metodo Delete

--- WebApiMapas/Controllers/MapasController.cs
+++ WebApiMapas/Controllers/MapasController.cs
@@ -2,5 +2,19 @@
 {
     public class MapasController
     {
+        /// <summary>
+        ///  Delete Api para deletar um georeferenciamento existente, caso o id não exista retorna NotFound, caso exista deleta e retorna NoContent
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<IActioResult> Delete(int id)
+        {
+            var existente = await _service.ObterPorId(id);
+            if (existente == null)
+                return NotFound();
+
+            await _service.Deletar(id);
+            return NoContent();
+        }
     }
 }
