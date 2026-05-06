1b0cab8dea1313a238f4e0224e84864d627a5d87
Marilene da Silva Araujo <maryllenearaujo@gmail.com>
2026-05-06 01:14:18 -0300

    Implementação da Interface  - ILocalizacaoRepository contendo os métodos inerentes ao contrato.

--- WebApiMapas/Repositories/Interfaces/ILocalizacaoRepository.cs
+++ WebApiMapas/Repositories/Interfaces/ILocalizacaoRepository.cs
@@ -1,6 +1,15 @@
 ﻿namespace WebApiMapas.Repositories.Interfaces
 {
+    /// <summary>
+    /// Interface que define os métodos para acessar e manipular dados 
+    /// de localização georreferenciada.
+    /// </summary>
     public interface ILocalizacaoRepository
     {
+        Task<List<Localizacao>> GetAll();   
+        Task<Localizacao> GetById(int id);
+        Task Add(Localizacao localizacao);
+        Task Update(Localizacao localizacao);
+        Task Delete(int id);
     }
 }
