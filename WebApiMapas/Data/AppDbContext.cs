9e8aa07b3425644e9122a1704830119d269d68b3
Nicolas de Oliveira <nicolas.lima2108@gmail.com>
2026-05-06 19:09:49 -0300

    Cumprimento do Contrato com a interface e adição do id ao Models.

--- WebApiMapas/Data/AppDbContext.cs
+++ WebApiMapas/Data/AppDbContext.cs
@@ -1,10 +1,11 @@
-﻿
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiMapas.Models;
 
-namespace ApiWebMapas.Data
+namespace WebApiMapas.Data
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Localizacao> Localizacoes { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
--- WebApiMapas/Repositories/LocalizacaoRepository.cs
+++ WebApiMapas/Repositories/LocalizacaoRepository.cs
@@ -1,5 +1,5 @@
-﻿using ApiWebMapas.Data;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiMapas.Data;
 using WebApiMapas.Models;
 using WebApiMapas.Repositories.Interfaces;
 
@@ -18,16 +18,52 @@ namespace WebApiMapas.Repositories
             _context = context;
         }
 
-       /// <summary>
-       /// 
-       /// </summary>
-       /// <param name="localizacao"></param>
-       /// <returns></returns>
+        public async Task<List<Localizacao>> GetAll()
+            => await _context.Localizacoes.ToListAsync();
+
+
+        public async Task<Localizacao> GetById(int id)
+            => await _context.Localizacoes.FindAsync(id);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="localizacao"></param>
+        /// <returns></returns>
+        /// 
         public async Task Add(Localizacao localizacao)
         {
             _context.Localizacoes.Add(localizacao);
             await _context.SaveChangesAsync();
         }
 
-    }
+        public async Task Update(Localizacao localizacao)
+        {
+            var existing = await _context.Localizacoes.FindAsync(localizacao.Id);
+
+            if (existing != null)
+            {
+                _context.Localizacoes.Update(localizacao);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Localização com ID {localizacao.Id} não encontrada.");
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            var localizacao = await _context.Localizacoes.FindAsync(id);
+            if (localizacao != null)
+            {
+                _context.Localizacoes.Remove(localizacao);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Localização com ID {id} não encontrada.");
+            }
+
+        }
 }
